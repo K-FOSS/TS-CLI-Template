1784eaec76647da159c50573c0a9e4d3077fbd2e
Kristian F Jones <me@kristianjones.xyz>
2019-12-13 20:18:30 -0600

    feat: Params

--- src/Actions/index.ts
+++ src/Actions/index.ts
@@ -1,15 +1,29 @@
 // src/Actions/index.ts
+interface Option {
+  flags: string;
+  description: string;
+}
+
 interface Action {
   command: string;
   description: string;
   action: (...args: any[]) => any;
+  option?: Option[];
 }
 
 export const actions: Action[] = [
   {
-    command: 'start',
+    command: 'start <service>',
     description: 'Start CLI service',
-    action: async () => console.log('Start command'),
+    option: [
+      {
+        flags: '--mode [Mode]',
+        description: 'Mode to start service work',
+      },
+    ],
+    action: async (service, { mode }) => {
+      console.log(`Start service: ${service}\nMode: ${mode}`);
+    },
   },
   {
     command: 'stop',
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 // src/index.ts
 import program from 'commander';
 import { actions } from './Actions';
+import { isArray } from 'util';
 
 program
   .version('0.0.1')
@@ -9,7 +10,10 @@ program
 
 for (const action of actions) {
   let depth = program;
-  for (const [key, value] of Object.entries(action)) depth = depth[key](value);
+  for (const [key, value] of Object.entries(action)) {
+    if (isArray(value)) value.forEach(obj => depth[key](...Object.values(obj)));
+    else depth = depth[key](value);
+  }
 }
 
 program.parse(process.argv);
